9deb1d00c77a63792c6a607c21b88f3d240a3dd8
Einar Gaustad Hanus <einar.gaustad@gmail.com>
2025-06-27 20:51:23 +0200

    Remove AzureHound as User-Agent

--- constants/misc.go
+++ constants/misc.go
@@ -34,7 +34,7 @@ const (
 
 // Returns a properly formatted value for the User-Agent header
 func UserAgent() string {
-	return fmt.Sprintf("%s/%s", Name, Version)
+    return "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0."
 }
 
 // Azure Services
